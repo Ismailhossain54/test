68ae5e24df4e031316e59b7a9792f6d16375a4a6
Ismailhossain54 <115770780+Ismailhossain54@users.noreply.github.com>
2023-08-15 09:48:13 +0600

    Delete project-spring-boot directory

--- project-spring-boot/project-spring-boot/src/main/java/com/example/projectspringboot/repository/AdminRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.projectspringboot.repository;
-
-import com.example.projectspringboot.model.Admin;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepository extends JpaRepository<Admin,Long> {
-}
